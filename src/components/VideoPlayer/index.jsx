5fa06a8057b49ccc83cb0c4063ef21b61b81d81a
LeeSangMin1029 <sangmins39@gmail.com>
2023-02-07 06:05:52 +0900

    useFetchVideo 적용

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,10 +1,13 @@
-import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import parse from "html-react-parser";
 import { VideoView } from "./styles";
+import { useFetchVideo } from "@/hooks";
 
 const VideoPlayer = () => {
-  const { state } = useLocation();
-  const { embedHtml } = state.player;
+  const [params] = useSearchParams();
+  const id = params.get("id");
+  const { video } = useFetchVideo(id, "1280", "714");
+  const { embedHtml } = video.player;
   const parseToComponent = (html) => parse(html);
   return <VideoView>{parseToComponent(embedHtml)}</VideoView>;
 };
