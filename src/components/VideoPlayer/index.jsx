4eac0ecd5da5e7903c6c6a08b01763952628d96f
LeeSangMin1029 <sangmins39@gmail.com>
2023-02-07 07:40:12 +0900

    채널 정보 내용 추가 및 스타일 적용

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,6 +1,13 @@
 import { useSearchParams } from "react-router-dom";
 import parse from "html-react-parser";
-import { VideoView } from "./styles";
+import {
+  PlayerBoard,
+  VideoDetail,
+  PlayerStyled,
+  ChannelInfo,
+  VideoInteraction,
+  BetweenContent,
+} from "./styles";
 import { useFetchVideo } from "@/hooks";
 
 const VideoPlayer = () => {
@@ -9,7 +16,32 @@ const VideoPlayer = () => {
   const { video } = useFetchVideo(id, "1280", "714");
   const { embedHtml } = video.player;
   const parseToComponent = (html) => parse(html);
-  return <VideoView>{parseToComponent(embedHtml)}</VideoView>;
+  return (
+    <PlayerBoard>
+      <PlayerStyled>{parseToComponent(embedHtml)}</PlayerStyled>
+      <VideoDetail>
+        <h1>{video.snippet.title}</h1>
+        <BetweenContent>
+          <ChannelInfo>
+            <a
+              href={`https://www.youtube.com/channel/UCtybqqaTj6Nx74Azdz1KrsA`}
+            >
+              <img src="https://yt3.ggpht.com/eyq-DtqUZb9kY1xHkTi1BrmBieCg8E7nI4hBA3_z_46dEBimEGxubgPevtuxUSyNOTIp8RWvOXg=s88-c-k-c0x00ffffff-no-rj" />
+            </a>
+            <div>
+              <a
+                href={`https://www.youtube.com/channel/UCtybqqaTj6Nx74Azdz1KrsA`}
+              >
+                채널 이름임 아무튼 채널 이름임
+              </a>
+              <p>구독자</p>
+            </div>
+          </ChannelInfo>
+          <VideoInteraction></VideoInteraction>
+        </BetweenContent>
+      </VideoDetail>
+    </PlayerBoard>
+  );
 };
 
 export default VideoPlayer;
--- src/components/VideoPlayer/styles.js
+++ src/components/VideoPlayer/styles.js
@@ -1,7 +1,55 @@
 import styled from "styled-components";
 
-export const VideoView = styled.div`
+export const PlayerBoard = styled.div`
   margin-left: 24px;
   padding-top: 24px;
   padding-right: 24px;
+  display: flex;
+  flex-direction: column;
+  height: 1300px;
+`;
+
+export const VideoDetail = styled.div`
+  margin-top: 12px;
+  margin-bottom: 24px;
+  h1 {
+    font-size: 20px;
+    font-weight: 600;
+  }
+`;
+
+export const PlayerStyled = styled.div``;
+
+export const ChannelInfo = styled.div`
+  width: 628px;
+  margin-right: 12px;
+  margin-top: 12px;
+  display: flex;
+  a img {
+    border-radius: 50%;
+    margin-right: 12px;
+    width: 40px;
+    height: 40px;
+  }
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    a {
+      line-height: 20px;
+    }
+    p {
+      line-height: 18px;
+      font-size: 12px;
+    }
+  }
+`;
+
+export const VideoInteraction = styled.div``;
+export const BetweenContent = styled.div`
+  width: 100%;
+  height: 55px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
 `;
