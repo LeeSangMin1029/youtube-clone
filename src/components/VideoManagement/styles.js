cb75f9ff730614f942d79df5b8529767769d62da
LeeSangMin1029 <sangmins39@gmail.com>
2023-02-08 16:56:56 +0900

    VideoManagement Component 추가

---
+++ src/components/VideoManagement/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 628px;
+  height: 43px;
+  margin-top: 12px;
+  display: flex;
+`;
+
+export const Feedback = styled.div`
+  display: flex;
+`;
+
+export const LikeButton = styled.div``;
+
+export const DisLikeButton = styled.div``;
+
+export const SharedButton = styled.div``;
+
+export const DownloadContainer = styled.div`
+  display: flex;
+`;
+
+export const OfflineButton = styled.div``;
+
+export const ClipButton = styled.div``;
+
+export const PlayerListSaveButton = styled.div``;
+
+export const MenuButton = styled.div``;
--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,24 +1,30 @@
 import { useSearchParams } from "react-router-dom";
 import parse from "html-react-parser";
+import VideoManangement from "@/components/VideoManagement";
 import {
   PlayerBoard,
   VideoDetail,
   PlayerStyled,
   ChannelInfo,
-  VideoInteraction,
   BetweenContent,
 } from "./styles";
 import { useFetchVideo } from "@/hooks";
 
+const WIDTH = "1268";
+const HEIGHT = "713";
+
 const VideoPlayer = () => {
   const [params] = useSearchParams();
   const id = params.get("id");
-  const { video } = useFetchVideo(id, "1280", "714");
+  const { video, isLoading } = useFetchVideo(id, WIDTH, HEIGHT);
+  if (isLoading) return <div>...Loading</div>;
   const { embedHtml } = video.player;
   const parseToComponent = (html) => parse(html);
   return (
     <PlayerBoard>
-      <PlayerStyled>{parseToComponent(embedHtml)}</PlayerStyled>
+      <PlayerStyled width={WIDTH} height={HEIGHT}>
+        {parseToComponent(embedHtml)}
+      </PlayerStyled>
       <VideoDetail>
         <h1>{video.snippet.title}</h1>
         <BetweenContent>
@@ -32,12 +38,12 @@ const VideoPlayer = () => {
               <a
                 href={`https://www.youtube.com/channel/UCtybqqaTj6Nx74Azdz1KrsA`}
               >
-                채널 이름임 아무튼 채널 이름임
+                아무튼 채널 이름임
               </a>
               <p>구독자</p>
             </div>
           </ChannelInfo>
-          <VideoInteraction></VideoInteraction>
+          <VideoManangement />
         </BetweenContent>
       </VideoDetail>
     </PlayerBoard>
