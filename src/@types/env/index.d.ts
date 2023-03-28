d8de498a7250db231116b8c375caf4e9ba38792b
LeeSangMin1029 <sangmins39@gmail.com>
2023-03-28 17:13:02 +0900

    secret_key 추가
    typescript 적용

---
+++ src/@types/env/index.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly VITE_HOST: string;
+  readonly VITE_API_KEY: string;
+  readonly VITE_SECRET_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,5 @@
 export const envConfig = {
   API_KEY: import.meta.env.VITE_API_KEY,
   HOST: import.meta.env.VITE_HOST,
+  SECRET_KEY: import.meta.env.VITE_SECRET_KEY,
 };
