a3dc6c263031ebe6e919ff122bf8bb1e17e7ae80
LeeSangMin1029 <sangmins39@gmail.com>
2023-03-30 22:57:56 +0900

    token 암호화 키

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,5 @@
 export const envConfig = {
   API_KEY: import.meta.env.VITE_API_KEY,
   HOST: import.meta.env.VITE_HOST,
-  SECRET_KEY: import.meta.env.VITE_SECRET_KEY,
+  ACCESS_SECRET_KEY: import.meta.env.VITE_ACCESS_SECRET_KEY,
 };
