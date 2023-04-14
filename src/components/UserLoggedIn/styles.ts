a6b985ec10f8dc9f9d509372000b406131cf974a
LeeSangMin1029 <sangmins39@gmail.com>
2023-04-14 20:20:24 +0900

    스타일 변경 및 추가

--- src/components/UserLoggedIn/styles.ts
+++ src/components/UserLoggedIn/styles.ts
@@ -1,25 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  width: 224px;
-  height: 40px;
-  display: flex;
-  justify-content: end;
-`;
-
-export const Interact = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: end;
-  height: 40px;
-`;
-
 export const SVGWrapper = styled.div`
   width: 40px;
   height: 40px;
   padding: 8px;
   margin-right: 8px;
 `;
+
 export const UserProfile = styled.div`
   > div {
     width: 32px;
