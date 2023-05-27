5e5a30783def8aaa717434f148a8419e8a6dcc0a
LeeSangMin1029 <sangmins39@gmail.com>
2023-05-28 02:53:23 +0900

    컴포넌트 적용 및 구독자 포맷 적용
    Player(YoutubePlayer) Component
    VideoDescription Component

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from 'react-router-dom';
-import YTPlayer from 'react-youtube';
 import VideoManangement from '@/components/VideoManagement';
+import Player from '@/components/Player';
+import VideoDescription from '@/components/VideoDescription';
 import {
   PlayerBoard,
   VideoDetail,
@@ -8,7 +9,7 @@ import {
   BetweenContent,
 } from './styles';
 import { useVideos } from '@/hooks';
-import { getVideoInfo } from '@/utils';
+import { getVideoInfo, getCountFormat } from '@/utils';
 
 const VideoPlayer = () => {
   const [params] = useSearchParams();
@@ -21,28 +22,19 @@ const VideoPlayer = () => {
     videoId,
     title,
     channelId,
-    viewCount,
     videoSrc,
     channelTitle,
+    description,
     subscriberCount,
+    viewCount,
+    publishedAt,
   } = getVideoInfo(video!);
-  console.log(video);
+
   return (
     <PlayerBoard>
-      {video && (
-        <>
-          <YTPlayer
-            className="player"
-            videoId={videoId}
-            opts={{
-              height,
-              width,
-              playerVars: {
-                autoplay: 1,
-                mute: 1,
-              },
-            }}
-          />
+      <>
+        <Player width={width} height={height} videoId={videoId} />
+        {video && (
           <VideoDetail>
             <h1>{title}</h1>
             <BetweenContent>
@@ -52,14 +44,19 @@ const VideoPlayer = () => {
                 </a>
                 <div>
                   <a href={channelId}>{channelTitle}</a>
-                  <p>구독자 {subscriberCount}</p>
+                  <p>구독자 {getCountFormat(subscriberCount, 1)}명</p>
                 </div>
               </ChannelInfo>
               <VideoManangement />
             </BetweenContent>
+            <VideoDescription
+              description={description}
+              viewCount={viewCount}
+              publishedAt={publishedAt}
+            />
           </VideoDetail>
-        </>
-      )}
+        )}
+      </>
     </PlayerBoard>
   );
 };
