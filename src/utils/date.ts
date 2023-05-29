266be355854f29d03ce591f9072ae7559a82aa7d
LeeSangMin1029 <sangmins39@gmail.com>
2023-05-30 04:06:09 +0900

    getDateSinceUpload 함수 변경

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,2 +1,26 @@
+export const DIVIDE_DATE = {
+  SECONDS: 60,
+  MINUTES: 60,
+  HOURS: 3600,
+  DAYS: 86400,
+  WEEKS: 604800,
+  MONTHS: 2592000,
+  YEARS: 31536000,
+} as const;
+type DIVIDE_DATE = (typeof DIVIDE_DATE)[keyof typeof DIVIDE_DATE];
+
+export const dateFormat = [
+  { compare: 60, unit: '방금', divide: DIVIDE_DATE.SECONDS },
+  { compare: 60, unit: '분', divide: DIVIDE_DATE.MINUTES },
+  { compare: 24, unit: '시간', divide: DIVIDE_DATE.HOURS },
+  { compare: 7, unit: '일', divide: DIVIDE_DATE.DAYS },
+  { compare: 5, unit: '주', divide: DIVIDE_DATE.WEEKS },
+  { compare: 12, unit: '개월', divide: DIVIDE_DATE.MONTHS },
+  { compare: 365, unit: '년', divide: DIVIDE_DATE.YEARS },
+];
+
 export const getOnlyDate = (date: string | Date) =>
   typeof date === 'string' ? new Date(date) : date;
+
+export const getSinceSeconds = (date: Date | string) =>
+  (new Date().getTime() - getOnlyDate(date).getTime()) / 1000;
--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,30 +1,11 @@
 import { YoutubeVideoItem } from '@/@types/youtube';
-import { getOnlyDate } from './date';
+import { getSinceSeconds, dateFormat } from './date';
 import crypto from 'crypto';
 
 export const getDateSinceUpload = (date: string | Date) => {
-  const milliSeconds = new Date().getTime() - getOnlyDate(date).getTime();
-
-  const seconds = milliSeconds / 1000;
-  if (seconds < 60) return `방금 전`;
-
-  const minutes = seconds / 60;
-  if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-
-  const hours = minutes / 60;
-  if (hours < 24) return `${Math.floor(hours)}시간 전`;
-
-  const days = hours / 24;
-  if (days < 7) return `${Math.floor(days)}일 전`;
-
-  const weeks = days / 7;
-  if (weeks < 5) return `${Math.floor(weeks)}주 전`;
-
-  const months = days / 30;
-  if (months < 12) return `${Math.floor(months)}개월 전`;
-
-  const years = days / 365;
-  return `${Math.floor(years)}년 전`;
+  const seconds = getSinceSeconds(date);
+  const since = dateFormat.filter((v) => seconds / v.divide < v.compare)[0];
+  return `${Math.floor(seconds / since.divide)}${since.unit} 전`;
 };
 
 export const randomKey = () =>
