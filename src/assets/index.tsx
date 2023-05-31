75cd1e6e44fa1aff7cd649bab11f771c2de9319f
LeeSangMin1029 <sangmins39@gmail.com>
2023-06-01 02:42:54 +0900

    LoadingSpinner svg 추가

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -2,3 +2,4 @@ export { ReactComponent as UploadIcon } from './video_upload.svg';
 export { ReactComponent as AlarmIcon } from './video_alarm.svg';
 export { ReactComponent as SettingIcon } from './app_settings.svg';
 export { ReactComponent as LoginIcon } from './user_login_profile.svg';
+export { ReactComponent as LoadingSpinner } from './loading.svg';
--- src/components/Player/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import YTPlayer from 'react-youtube';
-
-export const StyledYTPlayer = styled(YTPlayer)`
-  width: 1268px;
-  height: 713px;
-  background-color: black;
-`;
