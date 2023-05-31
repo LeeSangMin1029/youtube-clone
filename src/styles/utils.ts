6750c526bf1120e9310266e80b8f71736e33c4b5
LeeSangMin1029 <sangmins39@gmail.com>
2023-05-31 16:50:42 +0900

    조건에 따라 변경되는 CSS 함수 추가

--- src/styles/utils.ts
+++ src/styles/utils.ts
@@ -1,8 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const SVG = styled.div`
   width: 40px;
   height: 40px;
   padding: 8px;
   margin-right: 8px;
+`;
+
+export const conditionalContent = (delimiters: boolean) => css`
+  span {
+    color: ${delimiters ? '#606060' : 'inherit'};
+    :nth-child(even):before {
+      content: ${delimiters ? "'•'" : "''"};
+      margin: 0 4px;
+    }
+  }
 `;
