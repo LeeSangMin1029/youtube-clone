0ca5cecfcc6edccc42d8dc97ffa860d4d859764a
LeeSangMin1029 <sangmins39@gmail.com>
2023-06-03 11:29:24 +0900

    hamburger, youtube_logo 추가

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -3,3 +3,5 @@ export { ReactComponent as AlarmIcon } from './video_alarm.svg';
 export { ReactComponent as SettingIcon } from './app_settings.svg';
 export { ReactComponent as LoginIcon } from './user_login_profile.svg';
 export { ReactComponent as LoadingSpinner } from './loading.svg';
+export { ReactComponent as YoutubeLogo } from './youtube_logo.svg';
+export { ReactComponent as Hamburger } from './hamburger.svg';
