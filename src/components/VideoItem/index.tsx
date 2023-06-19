19c9796980898abb6b8df747747f0a30272c5db2
LeeSangMin1029 <sangmins39@gmail.com>
2023-06-19 22:49:29 +0900

    rerendering 방지
    컴포넌트의 위치가 변경되었지만 실제로 html의 계층구조는 변경되지 않도록 fragment로 감쌌다.

--- src/components/VideoItem/index.tsx
+++ src/components/VideoItem/index.tsx
@@ -4,9 +4,6 @@ import { VideoDetails, Description, YoutuberData, StyledDiv } from './styles';
 import { YoutubeVideo } from '@/@types/youtube';
 import ViewsWithDate from '@/components/ViewsWithDate';
 import VideoThumbnails from '@/components/VideoThumbnails';
-import { useMouseHandler } from '@/hooks';
-import YoutubeVideoPlayer from '@/components/YoutubeVideoPlayer';
-import CoverInteract from '@/components/CoverInteract';
 
 type VideoItemProps = {
   data: YoutubeVideo;
@@ -23,25 +20,18 @@ const VideoItem = ({ data }: VideoItemProps) => {
   const { channelId, channelTitle, title, thumbnails, publishedAt } = snippet;
   const channelHref = `https://www.youtube.com/channel/${channelId}`;
   const { url: channelThumb } = channel.snippet.thumbnails.default;
-  const { mouse, ...handler } = useMouseHandler();
   const targetLink = useNavigate();
   const onClick = () => {
     targetLink(`/watch?id=${id}`);
   };
 
   return (
-    <StyledDiv {...handler} onClick={onClick}>
-      <CoverInteract mouse={mouse} variant="rect" />
+    <StyledDiv onClick={onClick}>
       <VideoThumbnails
         id={id}
         thumbnails={thumbnails}
         duration={contentDetails.duration}
-        mouse={mouse}
-      >
-        {mouse.enter && (
-          <YoutubeVideoPlayer videoId={id} height={194} width={344} />
-        )}
-      </VideoThumbnails>
+      />
       <VideoDetails>
         <Link to={channelHref}>
           <img src={channelThumb} alt="testI" width="36px" height="36px" />
--- src/components/VideoThumbnails/index.tsx
+++ src/components/VideoThumbnails/index.tsx
@@ -1,31 +1,37 @@
 import { YoutubeVideoThumbnails } from '@/@types/youtube';
-import { MouseState } from '@/@types/global';
-import { ReactNode, memo } from 'react';
+import { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { Overlay, Thumbnails } from './styles';
 import { getDuration } from '@/utils';
+import { useMouseHandler } from '@/hooks';
+import YoutubeVideoPlayer from '@/components/YoutubeVideoPlayer';
+import CoverInteract from '@/components/CoverInteract';
 
 type VideoThumbnailsProps = {
   id: string;
   thumbnails: YoutubeVideoThumbnails;
   duration: string;
-  mouse: MouseState;
-  children: ReactNode;
 };
 
 const VideoThumbnails = memo(
-  ({ id, thumbnails, duration, mouse, children }: VideoThumbnailsProps) => {
+  ({ id, thumbnails, duration }: VideoThumbnailsProps) => {
+    const { mouse, ...handler } = useMouseHandler();
     return (
-      <Thumbnails mouse={mouse}>
-        <Link to={`/watch?id=${id}`}>
-          <img
-            src={thumbnails?.maxres?.url || thumbnails?.high?.url}
-            alt="thumbnails"
-          />
-          <Overlay>{getDuration(duration)}</Overlay>
-        </Link>
-        {children}
-      </Thumbnails>
+      <>
+        <CoverInteract {...handler} mouse={mouse} variant="rect" />
+        <Thumbnails mouse={mouse}>
+          <Link to={`/watch?id=${id}`}>
+            <img
+              src={thumbnails?.maxres?.url || thumbnails?.high?.url}
+              alt="thumbnails"
+            />
+            <Overlay>{getDuration(duration)}</Overlay>
+          </Link>
+          {mouse.enter && (
+            <YoutubeVideoPlayer videoId={id} height={194} width={344} />
+          )}
+        </Thumbnails>
+      </>
     );
   },
 );
